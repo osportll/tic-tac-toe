8e11d5996c82e474a3542b74324dd68e56c4ba86
Oscar <oscarportillo721@gmail.com>
2023-08-13 01:48:24 -0300

    Created the winnerChecker function to check if there is a winner player. The function uses the information from the boardArr array to create a new lettersArr with the strings of the textContent of the squares divs. After this, I created an array 'winningSequence' With all the possible winning sequences using the lettersArray indexes. A conditional loops through the sequences array and compares it to the lettersArray to see if one of the sequences is true, and then it logs the winner letter to the console

--- javascript.js
+++ javascript.js
@@ -2,24 +2,37 @@
 
 const Gameboard = (() => {
   const gameSquares = document.querySelectorAll('.testDiv');
+  const playerText = document.querySelector('.currentPlayer');
+  const restartBtn = document.querySelector('.restartBtn');
   const player1 = 'X';
   const player2 = 'O';
-  let currentPLayer = player1;
+  let currentPlayer = player1;
   let target;
+  let boardArr = [];
+  let winnerFound = false;
   /*  const boardArr = ['X', 'O']; */
+
+  gameSquares.forEach((element) => {
+    boardArr.push(element);
+  });
+
+  console.log(boardArr);
 
   function addMark(square) {
     gameSquares.forEach(() => {
       /* square.textContent = boardArr[0]; */
       if (square.textContent === '') {
-        square.textContent = currentPLayer;
-        if (currentPLayer === player1) {
-          currentPLayer = player2;
+        square.textContent = currentPlayer;
+        if (currentPlayer === player1) {
+          currentPlayer = player2;
         } else {
-          currentPLayer = player1;
+          currentPlayer = player1;
         }
+        playerText.textContent = `${currentPlayer}'s turn!`;
       }
     });
+
+    winnerChecker();
   }
 
   function addClickListener() {
@@ -32,7 +45,60 @@ const Gameboard = (() => {
     });
   }
 
-  return { addClickListener: addClickListener };
+  restartBtn.addEventListener('click', () => {
+    currentPlayer = player1;
+    playerText.textContent = `${currentPlayer}'s turn!`;
+    gameSquares.forEach((square) => {
+      square.textContent = '';
+    });
+  });
+
+  function winnerChecker() {
+    let lettersArr = [];
+
+    for (let i = 0; i <= 9; i++) {
+      if (boardArr[i] !== undefined) {
+        lettersArr.push(boardArr[i].textContent);
+      }
+    }
+
+    console.log(lettersArr);
+
+    let winningSequences = [
+      //These are the winning sequences for the Rows, Columns and Diagonals
+      [0, 1, 2], // row
+      [3, 4, 5], // row
+      [6, 7, 8], // row
+      [0, 4, 8], // diagonal
+      [2, 4, 6], // diagonal
+      [0, 3, 6], // column
+      [1, 4, 7], // column
+      [2, 5, 8], // column
+    ];
+
+    winningSequences.forEach((sequence) => {
+      const [a, b, c] = sequence;
+
+      if (
+        lettersArr[a] === lettersArr[b] &&
+        lettersArr[a] === lettersArr[c] &&
+        lettersArr[a] != '' &&
+        lettersArr[b] != '' &&
+        lettersArr[c] != ''
+      ) {
+        console.log('found winner! Winner is ' + lettersArr[a]);
+        winnerFound = true;
+      }
+
+      if (winnerFound) {
+        return;
+      }
+    });
+  }
+
+  return {
+    addClickListener: addClickListener,
+  };
 })();
 
 Gameboard.addClickListener();
